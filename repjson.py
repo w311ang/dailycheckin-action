bd44b7de0026c26bf58537e742fe58733d23b148
RZ <30381083+w311ang@users.noreply.github.com>
2021-06-08 10:19:35 +0800

    Delete repjson.py

--- repjson.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open('./config/config.json') as f:
-  raw=f.read()
-raw=raw.replace('$kgqq_cookie',os.getenv('kgqq_cookie'))
-print(raw)
-with open('./config/config.json','w') as f:
-  f.write(raw)
