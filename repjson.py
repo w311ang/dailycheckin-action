573b44b7ae41911c6c721773a1b87eccd9a7d063
RZ <30381083+w311ang@users.noreply.github.com>
2021-06-08 09:10:31 +0800

    Update repjson.py

--- repjson.py
+++ repjson.py
@@ -3,5 +3,6 @@ import os
 with open('./config/config.json') as f:
   raw=f.read()
 raw.replace('$kgqq_cookie',os.getenv('kgqq_cookie'))
+print(raw)
 with open('./config/config.json','w') as f:
   f.write(raw)
